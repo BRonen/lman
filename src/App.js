fdbf9c5107d8454faf789431a238bdde8ab78fb9
BRonen <brennopereira6@gmail.com>
2019-12-18 20:23:50 -0300

    pwa test

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import './App.css';
 
 import Routes from './Routes';
 
-function App() {
-
-  return (
-    <div className="app">
-      <Routes />
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <Routes />
+  </div>
+);
 
 export default App;
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 
 import Add from './pages/Add';
 import List from './pages/List';
@@ -9,15 +9,27 @@ import Update from './pages/Update';
 import Home from './pages/Home';
 import Main from './pages/Main';
 
+function isAuth() {
+	return false;
+}
 
-
-function Easter(){
-	return(
-		<div className="Content-box Easter">
-			<h1> hello world </h1>
-		</div>
+function PrivateRoute ({component: Component, ...props}){
+	return (
+		<Route {...props} render={() => (
+			isAuth() ? (
+				<Component/>
+			) : (
+				<Redirect to={{pathname: '/', state: {from: props.location} }}/>
+			)
+		)} />
 	);
 }
+
+const Easter = () => (
+	<div className="Content-box Easter">
+		<h1> hello world </h1>
+	</div>
+);
 
 export default function Routes(){
 	return(
@@ -29,6 +41,7 @@ export default function Routes(){
 				<Route path="/add" exact component={Add}/>
 				<Route path="/delete" exact component={Delete}/>
 				<Route path="/update" exact component={Update}/>
+				<PrivateRoute path="/app" exact component={() => (<h1>aaaaaaa</h1>)}/>
 				<Route path="/" component={Home}/>
 			</Switch>
 		</BrowserRouter>
